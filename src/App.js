cbf369472c7b5ccaa8207444b270999d1d648ab5
Phi Hai <dangphihai@gmail.com>
2020-08-23 11:38:44 +0700

    buoi2_styling_state_renderWithMap

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ import BaiTapLayout1 from './Components/BaiTapLayout1/BaiTapLayout1';
 import BaiTapLayout2 from './Components/BaiTapLayout2/BaiTapLayout2';
 import DataBinding from './DataBinding/DataBinding';
 import HandleEvent from './HandleEvent/HandleEvent';
+import Styling from './StylingComponent/Styling';
+import RenderWithState from './RenderWithState/RenderWithState';
+import BaiTapChonXe from './BaiTapChonXe/BaiTapChonXe';
+import RenderWithMap from './RenderWithMap/RenderWithMap';
 
 
 function App() {
   return (
     <div className="App">
-      <HandleEvent/>
+      {/* <HandleEvent/> */}
+      {/* <Styling/> */}
+      {/* <RenderWithState/> */}
+      {/* <BaiTapChonXe/> */}
+      <RenderWithMap/>
     </div>
   );
 }
